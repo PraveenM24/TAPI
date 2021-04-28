30216d250ecc52ecf275ae9bf4913dde42d6e04d
PraveenM24 <praveen050800@gmail.com>
2021-04-28 15:01:07 +0530

    Initial commit

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ require('dotenv').config();
 app.use(express.json());
 app.use(helmet());
 app.use('/', routes); //to use the routes
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/index.html');
+});
 app.use('/uploads', express.static('./uploads'));   
 app.use(compression());
 
@@ -21,7 +25,17 @@ const listener = app.listen(process.env.PORT || 3000, () => {
  //establish connection to database
  mongoose.connect(
      process.env.MONGODB_URI,
-     { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true},
+     {  useFindAndModify: false, 
+        useUnifiedTopology: true, 
+        useNewUrlParser: true, 
+        useCreateIndex: true,
+        server: { 
+            socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } 
+         }, 
+         replset: {
+            socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } 
+         }
+    },
      (err) => {
          if (err) return console.log("Error: ", err);
          console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
