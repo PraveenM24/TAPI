d053863db8ddd4a5328e895953bb43f629abd682
PraveenM24 <praveen050800@gmail.com>
2021-04-30 12:34:31 +0530

    Initial commit

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const compression = require('compression');
 
 require('dotenv').config();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 app.use(express.json());
 app.use(helmet());
 
